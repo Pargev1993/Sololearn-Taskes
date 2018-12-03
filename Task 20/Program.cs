a08982fd654c59e9501c8f7040005f52429358ae
pqo1993 <pqoalla@gmail.com>
2018-12-03 16:46:18 +0400

    Taskes

---
+++ Task 20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "sololearn";
+            string output = " ";
+            int first = input.IndexOf('l');
+            int last = input.IndexOf('e');
+            for (int i = first; i < last; i++)
+            {
+                output += input[i];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Task 21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21
+{
+    class Program
+    {
+        static int func(int a=3,int b=7,int c=6)
+        {
+            return a + b * c;
+        }
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 5;
+            int c = 2;
+            Console.WriteLine(func(c,b));
+        }
+    }
+}
