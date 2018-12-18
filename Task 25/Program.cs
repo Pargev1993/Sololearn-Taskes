7c8ad6c4e99b6b986f589031f6aac4dfc6900c58
pqo1993 <pqoalla@gmail.com>
2018-12-18 13:42:42 +0400

    Sololearn Tasks

---
+++ Task 25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                Console.WriteLine(i);
+                continue;
+            }
+        }
+    }
+}
---
+++ Task 26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "";
+            string y = "Sololearn";
+
+            foreach(char c in y)
+            {
+                if (c >= 'f')
+                {
+                    x += c;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
